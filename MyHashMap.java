af56ee9d5077ed42d6d4347c2a0f833dae179034
Riba33 <78846229+Riba33@users.noreply.github.com>
2021-05-06 09:26:24 +0300

    Update MyHashMap.java

--- MyHashMap.java
+++ MyHashMap.java
@@ -1,66 +1,120 @@
 public class MyHashMap<K,V> {
-    private Node<K,V> hashMap;
+    private Node<K,V>[] table =new Node[16];
     private int size = 0;
     private class Node<K,V> {
+        int hash;
         Object key;
         Object value;
         Node<K, V> next;
 
         Node(K key, V value, Node<K, V> next) {
+            this.hash = hash(key.hashCode());
             this.key = key;
             this.value = value;
             this.next = next;
         }
     }
-        public void put(Object key, Object value) { //добавляет пару ключ + значение
-
-            int val = 0;
-            if (hashMap == null) {
-                hashMap = new Node(key, value, null);
-                hashMap.next = hashMap;
-                size++;
+        public void put(K key, V value) { //добавляет пару ключ + значение
+            int hash = hash(key.hashCode());
+            int index = indexFor(hash, table.length);
+            if (size == table.length * 3 / 4) {
+                Node<K,V>[] newTable = new Node[table.length * 2];
+                transfer(newTable);
             }
-            else {
-                for(int i = 0; i <size; i++) {
-                    if (hashMap.key.equals(key)) {hashMap.value = value; val++; }
-                    hashMap = hashMap.next;
-                }
-                if (val == 0) {
-                    Node var = hashMap.next;
-                    hashMap.next = new Node(key,value, var);
-                    size++;
+            if (key == null) { putForNullKey(value); size++;}
+            else
+            { int val = 0;
+                for (Node<K,V> map: table)
+                {
+                    while (map != null)
+                    {
+                        if (map.hash == hash &&(key == map.key || key.equals(map.key)))
+                        {
+                            map.value = value;
+                            val++;
+                        }
+                        map = map.next;
                     }
-
                 }
+                if(val == 0) {addNode(hash,key,value,index); size++;}
+            }
         }
 
-        public void remove(Object key) { //удаляет пару по ключу
-            for (int i = 0; i < size; i++) {
-                if(hashMap.next.key.equals(key)){
-                    hashMap.next = hashMap.next.next;
-                } hashMap = hashMap.next;
-            } size--;
+        public void remove(Object key)
+        { //удаляет пару по ключу
+            int val = 0;
+            int hash = hash(key.hashCode());
+            int index = indexFor(hash(key.hashCode()), table.length);
+            Node<K,V> map = table[index];
+            if(map.hash == hash &&(key == map.key || key.equals(map.key)))
+            {
+                table[index] = map.next; size--;
+            }
         }
         public void clear() {//очищает коллекцию
-            hashMap = null;
-            size = 0;         //new Node<>(null,null,null)
+            table = new Node[16];
+            size = 0;
         }
         public int size() {//возвращает размер коллекции
             return size;
         }
         public Object get(Object key) { //возвращает значение(Object value) по ключу
             Object value = null;
-            for(int i = 0; i < size; i++) {
-                if (hashMap.key.equals(key)) value = hashMap.value;
+            int hash = hash(key.hashCode());
+            for(Node<K,V> map: table) {
+                while (map != null) {
+                    if (map.hash == hash &&(key == map.key || key.equals(map.key))) value = map.value;
+                    map = map.next;
+                }
             }
             return value;
         }
-        public String toString() {
+        static int hash(int h)
+        {
+            h ^= (h >>> 20) ^ (h >>> 12);
+            return h ^ (h >>> 7) ^ (h >>> 4);
+        }
+        static int indexFor(int h, int length)
+        {
+            return h & (length - 1);
+        }
+        void addNode(int hash, K key, V value, int index)
+        {
+            Node<K, V> e = table[index];
+            table[index] = new Node<K, V>(key, value, e);
+        }
+        void transfer(Node<K,V>[] newTable)
+        {
+            for(Node<K,V> map: table){
+                while (map != null) {
+                    newTable[indexFor(map.hash, newTable.length)] = new Node(map.key,map.value,null);
+                    map = map.next;
+                }
+            }
+            table = newTable;
+        }
+        void putForNullKey(V value){
+            Node<K,V> map = table[0];
+            int val = 0;
+            while (map != null) {
+                if(map.key == null) {
+                    map.value = value;
+                    val++;
+                }
+            }
+            if (val == 0) addNode(0,null,value,0);
+        }
+        public String toString()
+        {
             String s = "";
-            for (int i = 0; i < size; i++) {
-                s += hashMap.key + " " + hashMap.value + " ";
-                hashMap = hashMap.next;}
+            for (Node<K,V> map: table) {
+                while (map != null) {
+                    s += map.key + " " + map.value + " ";
+                    map = map.next;
+                }
+            }
             return s.trim();
         }
 }
+
 
